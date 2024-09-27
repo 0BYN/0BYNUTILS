1da5ccbf7e5717e358e47eb5c72535c80a28c9de
Gizmo <gizmoshiba@outlook.com>
2024-09-27 04:32:32 -0500

    fix: README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="obynutils",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "disnake",
@@ -13,6 +13,8 @@ setup(
         "quart-cors",
     ],
     description="A set of utility functions for Obyn bots",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="GizmoShiba",
     author_email="gizmoshiba@gmail.com",
     url="https://github.com/gizmoshiba/obyn-utils",
